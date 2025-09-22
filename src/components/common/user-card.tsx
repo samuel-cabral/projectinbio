404e06f34f659cb00185b8c856b41b79e799802d
Samuel Cabral <samuelcabral.mail@gmail.com>
2025-09-22 17:17:32 -0300

    feat: Adiciona componente UserCard com ícones de redes sociais e botão de compra, além de atualizar o componente Hero para integrá-lo na página inicial.

---
+++ src/components/common/user-card.tsx
@@ -0,0 +1,56 @@
+import { Github, Instagram, Linkedin, Plus, Twitter } from 'lucide-react'
+import Image from 'next/image'
+
+import { Button } from '../ui/button'
+
+export function UserCard() {
+  const icons = [Github, Instagram, Linkedin, Twitter, Plus]
+
+  return (
+    <div className="border-opacity-10 flex w-[348px] flex-col items-center justify-center gap-5 rounded-3xl border bg-[#121212] p-5 text-white">
+      <div className="size-48">
+        <Image
+          src="/me.png"
+          alt="Samuel Cabral"
+          className="h-full w-full rounded-full object-cover"
+          width={192}
+          height={192}
+        />
+      </div>
+
+      <div className="flex w-full flex-col gap-2">
+        <div className="flex items-center gap-2">
+          <h3 className="min-w-0 overflow-hidden text-3xl font-bold">
+            Samuel Cabral
+          </h3>
+        </div>
+
+        <p className="opacity-40">
+          &quot;Eu faço produtos para a Internet&quot;
+        </p>
+      </div>
+
+      <div className="flex w-full flex-col gap-2">
+        <span className="text-xs font-medium uppercase">Links</span>
+        <div className="flex gap-3">
+          {icons.map((Icon, index) => (
+            <button
+              key={index}
+              className="rounded-xl bg-[#1E1E1E] p-3 hover:bg-[#2E2E2E]"
+            >
+              <Icon />
+            </button>
+          ))}
+        </div>
+        <div className="flex h-[172px] w-full flex-col gap-3">
+          <div className="flex w-full flex-col items-center gap-3">
+            <Button className="w-full">Template SaaS - Compre agora</Button>
+            <button className="rounded-xl bg-[#1E1E1E] p-3 hover:bg-[#2E2E2E]">
+              <Plus />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/components/landing-page/hero.tsx
+++ src/components/landing-page/hero.tsx
@@ -1,7 +1,11 @@
+import { UserCard } from '../common/user-card'
+import { Button } from '../ui/button'
+import { Input } from '../ui/input'
+
 export function Hero() {
   return (
-    <div className="flex border">
-      <div className="mt-[35vh] flex w-full flex-col gap-2 border">
+    <div className="flex h-screen">
+      <div className="mt-[35vh] flex w-full flex-col gap-2">
         <h1 className="text-5xl leading-[64px] font-bold text-white">
           Seus projetos e redes sociais em um único link
         </h1>
@@ -11,16 +15,16 @@ export function Hero() {
           Acompanhe o engajamento com Analytics de clicks.
         </h2>
 
-        <div className="-mt-[10vh] flex w-full items-center gap-2">
-          <span className="text-xl text-white">projectinbio.com</span>
-          <input type="text" />
-          <button>Criar agora</button>
+        <div className="mt-[10vh] flex w-full items-center gap-2">
+          <span className="text-xl text-white">projectinbio.com/</span>
+          <Input type="text" placeholder="Seu link" />
+          <Button>Criar agora</Button>
         </div>
       </div>
 
-      <div className="bg-[radial-gradient(circle_at_50%_50%,#4B2DBB, transparent_55%)] flex w-full items-center justify-center">
+      <div className="flex w-full items-center justify-center bg-[radial-gradient(circle_at_50%_50%,#4B2DBB,transparent_55%)]">
         <div className="relative">
-          {/* <UserCard /> */}
+          <UserCard />
           <div className="absolute -right-[45%] -bottom-[7%]">
             {/* <TotalVisits /> */}
           </div>
