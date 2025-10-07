f9fbc3b4918b9aaa9cce45d785df1245e7c8364f
Samuel Cabral <samuelcabral.mail@gmail.com>
2025-10-07 18:54:15 -0300

    feat: Atualiza o componente Input na seção Hero para incluir bordas personalizadas, melhorando a estética e a consistência visual da página inicial.

--- src/components/landing-page/hero.tsx
+++ src/components/landing-page/hero.tsx
@@ -19,7 +19,11 @@ export function Hero() {
 
         <div className="mt-[10vh] flex w-full items-center gap-2">
           <span className="text-xl text-white">projectinbio.com/</span>
-          <Input type="text" placeholder="Seu link" className="h-12" />
+          <Input
+            type="text"
+            placeholder="Seu link"
+            className="border-background bg-background h-12"
+          />
           <Button className="h-12">Criar agora</Button>
         </div>
       </div>
