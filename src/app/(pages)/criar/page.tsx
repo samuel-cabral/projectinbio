2da3318e2504ce9921433a97e4e63ceeec58b173
Samuel Cabral <samuelcabral.mail@gmail.com>
2025-10-07 18:57:15 -0300

    feat: Reestrutura a página inicial, movendo componentes para a nova estrutura de pastas e adicionando estilos globais, melhorando a organização e a estética do projeto.

---
+++ src/app/(pages)/criar/page.tsx
@@ -0,0 +1,32 @@
+import { Rocket } from 'lucide-react'
+
+import { Header } from '@/components/landing-page/header'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+
+export default function Criar() {
+  return (
+    <div>
+      <Header />
+      <div className="mx-auto flex h-screen max-w-xl flex-col items-center justify-center gap-10">
+        <div className="flex items-center gap-4">
+          <h1 className="text-4xl font-bold">Escolha seu link</h1>
+          <Rocket className="size-10" />
+        </div>
+
+        <form action="" className="mt-5 flex w-full items-center gap-2">
+          <span>projectinbio/</span>
+          <Input
+            type="text"
+            className="border-accent bg-accent h-12 w-full rounded-xl"
+          />
+          <Button className="h-12 w-[126px]">Criar</Button>
+        </form>
+
+        <div>
+          <span className="text-destructive">Erro de exemplo</span>
+        </div>
+      </div>
+    </div>
+  )
+}
