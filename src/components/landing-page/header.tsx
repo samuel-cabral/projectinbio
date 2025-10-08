777ad98d27ef917ccdf5955dffc15f60e977e4a0
Samuel Cabral <samuelcabral.mail@gmail.com>
2025-10-07 21:01:27 -0300

    feat: Atualiza o componente Header para posicionar o elemento no topo da página, melhorando a estética e a usabilidade da navegação.

--- src/components/landing-page/header.tsx
+++ src/components/landing-page/header.tsx
@@ -4,7 +4,7 @@ import { Button } from '../ui/button'
 
 export function Header() {
   return (
-    <div className="absolute right-0 left-0 mx-auto flex max-w-7xl items-center justify-between py-10">
+    <div className="absolute top-0 right-0 left-0 mx-auto flex max-w-7xl items-center justify-between py-10">
       <div className="flex items-center gap-4">
         <Image src="/icon.svg" alt="ProjectInBio Logo" width={32} height={32} />
         <h3 className="text-2xl font-bold text-white">ProjectInBio</h3>
