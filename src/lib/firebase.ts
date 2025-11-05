63c39d33f8bb0ae32bd76141d224d27fc2391884
Samuel Cabral <samuelcabral.mail@gmail.com>
2025-11-05 07:53:43 -0300

    feat: inicializar firebase no app

---
+++ src/lib/firebase.ts
@@ -0,0 +1,29 @@
+import 'server-only'
+
+import { cert, getApps, initializeApp } from 'firebase-admin/app'
+import { getFirestore } from 'firebase-admin/firestore'
+import { getStorage } from 'firebase-admin/storage'
+
+// Certificado do Firebase
+const decodedPrivateKey = Buffer.from(
+  process.env.FIREBASE_PRIVATE_KEY!,
+  'base64'
+).toString('utf-8')
+
+export const firebaseCert = cert({
+  projectId: process.env.FIREBASE_PROJECT_ID!,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
+  privateKey: decodedPrivateKey,
+})
+
+// Instancia do app
+if (!getApps().length) {
+  initializeApp({
+    credential: firebaseCert,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET!,
+  })
+}
+
+export const db = getFirestore()
+
+export const storage = getStorage().bucket()
